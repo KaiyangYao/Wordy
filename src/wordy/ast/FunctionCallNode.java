865a020da21ae724a5994f235fbed72662d91df0
Kaiyang Yao <yaoky2008@gmail.com>
2022-12-08 21:45:59 -0600

    update FunctionCallNode

--- src/wordy/ast/FunctionCallNode.java
+++ src/wordy/ast/FunctionCallNode.java
@@ -1,6 +1,43 @@
 package wordy.ast;
 
-public class FunctionCallNode {
-    // Needs to know the FunctionNode block it should run, and what parameters it's getting passed
-    
+import wordy.interpreter.EvaluationContext;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class FunctionCallNode extends ExpressionNode {
+    private final VariableNode name;
+    private final List<VariableNode> params;
+
+    public FunctionCallNode(VariableNode name, List<VariableNode> parameters) {
+        this.name = name;
+        this.params = parameters;
+    }
+
+    @Override
+    public Map<String, ASTNode> getChildren() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    protected double doEvaluate(EvaluationContext context) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "FunctionCallNode{name=" + name + " params=" + params.toString() + "}";
+    }
 }
